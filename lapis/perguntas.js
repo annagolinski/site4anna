5dc125db7988281329205de8517c1e3a656eb8e9
annagolinski <anna.golinski.santos@escola.pr.gov.br>
2024-10-10 11:27:41 -0300

    Add files via upload

---
+++ lapis/perguntas.js
@@ -0,0 +1,38 @@
+criaCartao(
+    'TVD',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'TVD',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'TVD',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'TVD',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'TVD',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+   'TVD' 
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'TVD' 
+     'Qual irmão Salvatore fica com Elena no final da série?'
+    'Damon'
+)
