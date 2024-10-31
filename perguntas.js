19820175a18b7872001f5b3e88d654e11e8607f7
annagolinski <anna.golinski.santos@escola.pr.gov.br>
2024-10-31 11:15:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,38 @@
+criaCartao(
+    'TVD',
+    'Em qual cidade se passa a história?',
+    'Mystic Falls'
+    );
+
+criaCartao(
+    'TVD',
+    'Como é o nome do Original que Stefan e Damon descendem?',
+    'Klaus Mikaelson'
+);
+criaCartao(
+    'TVD',
+    'Qual é o nome do pais dos vampiros originais?',
+    'Mikael'
+);
+
+criaCartao(
+    'TVD',
+    'Em qual temporada Elena Gilbert se transforma vampira?',
+    'quarta temporada'
+);
+criaCartao(
+    'TVD',
+    'Quem matou Lexi, a melhor amiga de Stefan?',
+    'Damon'
+);
+
+criaCartao(
+    'TVD' ,
+     'Qual irmão Salvatore fica com Elena no final da série?',
+     'Ketherine Pierce'
+);
+criaCartao(
+    'TVD' ,
+     'Qual irmão Salvatore fica com Elena no final da série?',
+    'Damon',
+);
